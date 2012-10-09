91601411da97e1a9871f6e3623eb0c7620cffe1d
Guido Stevens <guido.stevens@cosent.nl>
2012-10-09 13:20:46 +0200

    reindent for better pep8

--- setup.py
+++ setup.py
@@ -13,32 +13,33 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
-setup(name='plonesocial.microblog',
-      version=version,
-      description="Simple microblogging in Plone",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plonesocial.microblog',
+    version=version,
+    description="Simple microblogging in Plone",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
         ],
-      keywords='plone socbiz social microblog',
-      author='Guido Stevens',
-      author_email='guido.stevens@cosent.net',
-      url='http://github.com/cosent/plonesocial.microblog',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plonesocial'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
+    keywords='plone socbiz social microblog',
+    author='Guido Stevens',
+    author_email='guido.stevens@cosent.net',
+    url='http://github.com/cosent/plonesocial.microblog',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plonesocial'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
       # -*- Entry points: -*-
   	  [z3c.autoinclude.plugin]
   	  target = plone
       """,
-      )
+    )
